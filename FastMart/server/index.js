b5845fd7377161a303b5f1115228ee9a7b9d1b26
Sandip-Maity-2023 <12sandip125@gmail.com>
2026-07-06 00:24:01 +0530

    MERN

--- FastMart/server/index.js
+++ FastMart/server/index.js
@@ -88,7 +88,7 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 app.use('/api/payment', require('./routes/paymentRoutes'));
 app.use('/api/analytics', require('./routes/analyticsRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/products',require('./routes/productRoutes'));
+// app.use('/api/products',require('./routes/productRoutes'));
 //app.use('/api/ai', require('./routes/aiRoutes'));
 
 // Root Route
